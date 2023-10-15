04ae2e68a537460cbf4894c20e7bf9b228d0335c
suhail <suhail@suhails-MacBook-Air.local>
2023-10-16 02:38:40 +0530

    final commit

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://ecommerce-backend-qxoh.onrender.com' }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
